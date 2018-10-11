1bf83764e0014919f2743cf87a0a2469638d33ee
Lassi Kortela <lassi@lassi.io>
2018-10-11 21:05:41 +0300

    Fill in template

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,29 @@
 import {
   Beautifier,
-  Language,
   BeautifierBeautifyData,
   DependencyType,
+  ExecutableDependency,
 } from "unibeautify";
 import * as readPkgUp from "read-pkg-up";
 import options from "./options";
 
 const { pkg } = readPkgUp.sync({ cwd: __dirname });
 export const beautifier: Beautifier = {
-  name: "{{ beautifierFancyTitle }}",
+  name: "ocp-indent",
   package: pkg,
   dependencies: [
     {
-      type: DependencyType.{{ beautifierDependencyType }},
-      name: "{{ beautifierFancyTitle }}",
-      package: "{{ beautifierNpmPackage }}", // remove this line if executable
-      program: "{{ beautifierExeCommand }}", // remove this line if npm
-      parseVersion: [],
-      homepageUrl: "{{ beautifierHomepageUrl }}",
-      installationUrl: "{{ beautifierInstallUrl }}",
-      bugsUrl: "{{ beautifierBugsUrl }}",
+      type: DependencyType.Executable,
+      name: "ocp-indent",
+      program: "ocp-indent",
+      homepageUrl: "https://github.com/OCamlPro/ocp-indent",
+      installationUrl: "https://www.typerex.org/ocp-indent.html",
+      bugsUrl: "https://github.com/OCamlPro/ocp-indent/issues",
       badges: []
     },
   ],
   options: {
-    
-  },
-  resolveConfig: () => {
-
+    OCaml: {},
   },
   beautify({
     text,
@@ -38,9 +33,15 @@ export const beautifier: Beautifier = {
     dependencies,
     beautifierConfig,
   }: BeautifierBeautifyData) {
-    return new Promise<string>((resolve, reject) => {
-
-    });
+    const executable = dependencies.get<ExecutableDependency>("ocp-indent");
+    return executable
+      .run({ args: [], stdin: text, options: {} })
+      .then(({ exitCode, stderr, stdout }) => {
+        if (exitCode) {
+          return Promise.reject(stderr);
+        }
+        return Promise.resolve(stdout);
+      });
   },
 };
 export default beautifier;
